623ae0c948e7ebba66c2bab50499ea782502f5c2
Ian Hipolito <ian.hipolito@sap.com>
2023-11-29 03:44:27 +0000

    Modifications to db.sql

--- db.sql
+++ db.sql
@@ -5,58 +5,22 @@ CREATE DATABASE student;
 USE student;
 
 CREATE TABLE students (
-    studentID INT NOT NULL AUTO_INCREMENT,
+    studentID INT NOT NULL AUTOINCREMENT,
     studentName VARCHAR(255),
-    email VARCHAR(255),
     course VARCHAR(255),
     year INT,
     PRIMARY KEY(studentID)
 );
 
-DELIMITER //
-CREATE PROCEDURE AddStudent(IN p_studentName VARCHAR(255), IN p_course VARCHAR(255), IN p_year INT)
-BEGIN
-    INSERT INTO students (studentName, course, year) VALUES (p_studentName, p_course, p_year);
-END //
-DELIMITER ;
+INSERT INTO students (studentName, course, year) VALUES (p_studentName, p_course, p_year);
 
-DELIMITER //
-CREATE PROCEDURE GetAllStudents()
-BEGIN
-    SELECT * FROM students;
-END //
-DELIMITER ;
+SELECT * FROM students;
 
-DELIMITER //
-CREATE PROCEDURE GetStudentByID(IN p_studentID INT)
-BEGIN
-    SELECT * FROM students WHERE studentID = p_studentID;
-END //
-DELIMITER ;
+SELECT * FROM students WHERE studentID = p_studentID;
 
-DELIMITER //
-CREATE PROCEDURE UpdateStudent(IN p_studentID INT, IN p_studentName VARCHAR(255), IN p_course VARCHAR(255), IN p_year INT)
-BEGIN
-    UPDATE students SET studentName = p_studentName, course = p_course, year = p_year WHERE studentID = p_studentID;
-END //
-DELIMITER ;
+UPDATE students SET studentName = p_studentName, course = p_course, year = p_year WHERE studentID = p_studentID;
 
-DELIMITER //
-CREATE PROCEDURE DeleteStudent(IN p_studentID INT)
-BEGIN
-    DELETE FROM students WHERE studentID = p_studentID;
-END //
-DELIMITER ;
+DELETE FROM students WHERE studentID = p_studentID;
 
 
-CALL AddStudent('John Doe', 'Computer Science', 2023);
-CALL AddStudent('Jane Smith', 'Biology', 2022);
-
-CALL GetAllStudents();
-
-CALL GetStudentByID(1);
-
-CALL UpdateStudent(1, 'John Updated', 'Mathematics', 2024);
-
-CALL DeleteStudent(1);
 
